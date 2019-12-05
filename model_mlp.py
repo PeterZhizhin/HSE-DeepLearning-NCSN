b4e240124e3f81ca6cdcc3d9b2d6bc1c5f73e6f6
anniesss1 <dayanamuha@gmail.com>
2019-12-05 17:50:09 +0300

    Add files via upload

---
+++ model_mlp.py
@@ -0,0 +1,14 @@
+class ModelMLP(object):
+	"""docstring for ModelMLP"""
+	def __init__(self, hidden_size=128):
+		super(ModelMLP, self).__init__()
+		self.layers = nn.Sequential(
+			nn.Linear(2, hidden_size),
+			nn.Softplus(),
+			nn.Linear(hidden_size, hidden_size),
+			nn.Softplus(),
+			nn.Linear(hidden_size, 2)
+		)
+
+	def forward(self, x):
+		return self.layers(x)
